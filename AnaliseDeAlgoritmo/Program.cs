b3eb93c396795938d64e0b3c039236e993eaf9e4
Miguel <miguel.reis@tripletech.com.br>
2019-07-24 14:30:03 -0300

    updated: métodos da heap

--- AnaliseDeAlgoritmo/Program.cs
+++ AnaliseDeAlgoritmo/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using AnaliseDeAlgoritmo.Heap;
 using AnaliseDeAlgoritmo.Sorts;
+using System;
 
 namespace AnaliseDeAlgoritmo
 {
@@ -16,11 +17,22 @@ namespace AnaliseDeAlgoritmo
          */
         static void Main(string[] args)
         {
-            int[] array = { 15, 5, 1, 3, 9, 7, 2, 6, 12, 11 };
+            int[] array = { 3, 1, 5, 8, 2, 4, 7, 6, 9 };
 
             Utils.Print(array);
-            MergeSort.Sort(array);
-            Utils.Print(array);
+            Heap.Heap heap = new Heap.Heap();
+
+            heap.ConstroiHeap(array);
+            Utils.Print(heap.H.ToArray());
+
+            heap.RemoveDaHeap();
+            Utils.Print(heap.H.ToArray());
+
+            heap.InserenaHeap(10, 10);
+            Utils.Print(heap.H.ToArray());
+
+            heap.AlteraHeap(3, 100);
+            Utils.Print(heap.H.ToArray());
 
             Console.ReadLine();
         }
