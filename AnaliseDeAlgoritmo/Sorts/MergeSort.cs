29a544c874e7dd0f8d4aa8a711967b75f8c4cbfb
Miguel <miguel.reis@tripletech.com.br>
2019-08-07 08:20:41 -0300

    updated: MergeSort com função de comparação

--- AnaliseDeAlgoritmo/Sorts/MergeSort.cs
+++ AnaliseDeAlgoritmo/Sorts/MergeSort.cs
@@ -6,9 +6,9 @@ namespace AnaliseDeAlgoritmo.Sorts
 {
     public static class MergeSort
     {
-        public static void Sort<T>(IList<T> array, Func<T, T, bool> comparingFunction)
+        public static void Sort<T>(this IList<T> array, Func<T, T, bool> comparingFunction)
         {
-            SortAndMerge<T>(array, 0, array.Count, comparingFunction);
+            SortAndMerge<T>(array, 0, array.Count - 1, comparingFunction);
         }
 
         private static void SortAndMerge<T>(IList<T> array, int start, int end, Func<T, T, bool> comparingFunction)
@@ -41,7 +41,7 @@ namespace AnaliseDeAlgoritmo.Sorts
 
             while (i < sizeL && j < sizeR)
             {
-                if(comparingFunction(L[i], R[j]))
+                if (comparingFunction(L[i], R[j]))
                 {
                     array[k] = L[i];
                     i++;
@@ -69,67 +69,9 @@ namespace AnaliseDeAlgoritmo.Sorts
             }
         }
 
-        public static void Sort(int[] array)
+        public static void Sort(this int[] array)
         {
-            SortAndMerge(array, 0, array.Length - 1);
-        }
-
-        private static void SortAndMerge(int[] array, int start, int end)
-        {
-            if (start < end)
-            {
-                int middle = start + (end - start) / 2;
-
-                SortAndMerge(array, start, middle);
-                SortAndMerge(array, middle + 1, end);
-
-                Merge(array, start, middle, end);
-            }
-        }
-
-        private static void Merge(int[] array, int start, int middle, int end)
-        {
-            int sizeL = middle - start + 1;
-            int sizeR = end - middle;
-
-            int[] L = new int[sizeL];
-            int[] R = new int[sizeR];
-
-            for (int h = 0; h < sizeL; h++)
-                L[h] = array[start + h];
-            for (int h = 0; h < sizeR; h++)
-                R[h] = array[middle + h + 1];
-
-            int i = 0, j = 0, k = start;
-
-            while (i < sizeL && j < sizeR)
-            {
-                if (L[i] <= R[j])
-                {
-                    array[k] = L[i];
-                    i++;
-                }
-                else
-                {
-                    array[k] = R[j];
-                    j++;
-                }
-                k++;
-            }
-
-            while (i < sizeL)
-            {
-                array[k] = L[i];
-                i++;
-                k++;
-            }
-
-            while (j < sizeR)
-            {
-                array[k] = R[j];
-                j++;
-                k++;
-            }
+            array.Sort<int>((a, b) => a < b);
         }
     }
 }
