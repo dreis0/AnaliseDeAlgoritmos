dbb4fb72bbfe7b48337588a02604589967b6c6e8
Miguel <miguel.reis@tripletech.com.br>
2019-08-08 09:30:42 -0300

    added: extension methods uteis

---
+++ AnaliseDeAlgoritmo/Utils/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnaliseDeAlgoritmo
+{
+    public static class Extensions
+    {
+        public static IList<T> Swap<T>(this IList<T> array, int firstIndex, int SecondIndex)
+        {
+            T aux = array[firstIndex];
+            array[firstIndex] = array[SecondIndex];
+            array[SecondIndex] = aux;
+
+            return array;
+        }
+
+        public static IList<int> Randomize(this IList<int> array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Count; i++)
+                array[i] = rnd.Next(0, 100);
+
+            return array;
+        }
+
+        public static IList<double> Randomize(this IList<double> array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Count; i++)
+                array[i] = rnd.NextDouble() * 100;
+
+            return array;
+        }
+    }
+}
